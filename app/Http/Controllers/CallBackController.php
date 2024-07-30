2015d79319175fb554759d9e66ca1293c47bd2df
LuckyKing0307 <jasaergasheva@gmail.com>
2024-07-30 16:31:02 +0500

    Items Btns

--- app/Http/Controllers/CallBackController.php
+++ app/Http/Controllers/CallBackController.php
@@ -83,7 +83,7 @@ class CallBackController extends Controller
                     'chat_id' => $request['user_id'],
                     'caption' => 'Photo',
                     'reply_markup' => $reply_markup,
-                    'photo' => $file->isFileRemote().'  '.$file->getFilename(),
+                    'photo' => $file,
                 ];
                 try {
                     $this->telegram::sendPhoto($messageData);
