563a6bd43869ab34c19762aff79098e2ba8eb5c5
LuckyKing0307 <jasaergasheva@gmail.com>
2025-06-29 23:54:48 +0500

    Items Btns

--- app/Orchid/Screens/AdminConfigs.php
+++ app/Orchid/Screens/AdminConfigs.php
@@ -65,8 +65,8 @@ class AdminConfigs extends Screen
         return [
             ConfigsTable::class,
             Layout::modal('createTrigger', CreateRows::class)
-            ->title('Create Trigger')
-            ->applyButton('Add Trigger'),
+                ->title('Create Trigger')
+                ->applyButton('Add Trigger'),
             Layout::modal('editConfig', EditConfigRows::class)->async('asyncGetConfig'),
             Layout::modal('editItemConfig', EditItemConfigRows::class)->async('asyncGetConfig')
         ];
@@ -75,7 +75,7 @@ class AdminConfigs extends Screen
     public function asyncGetConfig(AdConfigs $config): array
     {
         $data = json_decode($config->data, 1);
-        if ($config->type==='section_item'){
+        if ($config->type === 'section_item') {
             $configDb = [
                 'id' => $config->id,
                 'name' => $config->name,
@@ -83,7 +83,7 @@ class AdminConfigs extends Screen
                 'item_text_bg' => isset($data['text']) ? $data['text']['bg'] : '',
                 'price' => isset($data['price']) ? $data['price'] : '',
             ];
-        }else{
+        } else {
             $configDb = [
                 'id' => $config->id,
                 'name' => $config->name,
@@ -143,23 +143,28 @@ class AdminConfigs extends Screen
     public function edit(Request $request)
     {
         $config = AdConfigs::find($request->input('trigger.id'));
-        if ($config->type==='section_item'){$request->validate([
-            'trigger.item_text_en' => 'required',
-            'trigger.item_text_bg' => 'required',
-            'trigger.price' => 'required',
-        ]);
-            $this->changeSectionItem($request,$config);
-        }else{
+        if ($config->type === 'menu') {
+            $this->changeMenuSection($request, $config);
+        }
+        if ($config->type === 'section_item') {
+            $request->validate([
+                'trigger.item_text_en' => 'required',
+                'trigger.item_text_bg' => 'required',
+                'trigger.price' => 'required',
+            ]);
+            $this->changeSectionItem($request, $config);
+        } else {
             $request->validate([
                 'trigger.text_en' => 'required',
                 'trigger.text_bg' => 'required',
                 'trigger.fields' => 'required',
             ]);
-            $this->changeSection($request,$config);
+            $this->changeSection($request, $config);
         }
     }
 
-    public function changeSectionItem(Request $request,AdConfigs $config){
+    public function changeSectionItem(Request $request, AdConfigs $config)
+    {
         $photoUrl = [];
         $photoIds = $request->input('trigger.photo', []) ? $request->input('trigger.photo', []) : [];
         foreach ($photoIds as $photoId) {
@@ -179,7 +184,9 @@ class AdminConfigs extends Screen
 
     }
 
-    public function changeSection(Request $request, $config){
+
+    public function changeSection(Request $request, $config)
+    {
 
         $fields = [];
 
@@ -192,11 +199,11 @@ class AdminConfigs extends Screen
                     'function' => '',
                     'trigger_en' => $trigger['en'],
                     'trigger_bg' => $trigger['bg'],
-                    'type' => 'section_item',
+                    'type' => 'section',
                     'fields' => '[]',
                     'data' => json_encode($data),
                 ]);
-            }else{
+            } else {
                 $sections = $configGet->get()->first();
                 $sections->update([
                     'trigger_en' => $trigger['en'],
@@ -211,6 +218,48 @@ class AdminConfigs extends Screen
             'fields' => $fields,
         ];
         $config->data = json_encode($data);
+        $config->save();
+    }
+
+    public function changeMenuSection(Request $request, $config)
+    {
+        $currentData = json_decode($config->data, true) ?? [];
+        $storedFields = $currentData['fields'] ?? [];
+        $incomings = $request->input('trigger.fields');
+        $updatedFields = [];
+
+        foreach ($incomings as $key => $incoming) {
+                $t = $incoming;
+                $field = AdConfigs::where('id',$key);
+                if (isset($storedFields[$key]) and $field->exists()) {
+                    $id = $key;
+                    AdConfigs::find($key)->update([
+                        'name' => $t['en'],
+                        'trigger_en' => $t['en'],
+                        'trigger_bg' => $t['bg'],
+                    ]);
+                } else {
+                    $section = AdConfigs::create([
+                        'name' => $t['en'],
+                        'function' => 'sectionData',
+                        'trigger_en' => $t['en'],
+                        'trigger_bg' => $t['bg'],
+                        'type' => 'section',
+                        'fields' => '[]',
+                        'data' => json_encode([]),
+                    ]);
+                    $id = $section->id;
+                }
+
+                $updatedFields[$id] = ['en' => $t['en'], 'bg' => $t['bg']];
+        }
+        $currentData['text'] = [
+            'en' => $request->input('trigger.text_en'),
+            'bg' => $request->input('trigger.text_bg'),
+        ];
+        $currentData['fields'] = $updatedFields;
+
+        $config->data = json_encode($currentData, JSON_UNESCAPED_UNICODE);
         $config->save();
     }
 
