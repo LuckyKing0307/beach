d9b769e58c49500184d8ce43b0e34ff589a6cc93
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-23 18:05:30 +0500

    Items Btns

--- resources/views/realtimechat.blade.php
+++ resources/views/realtimechat.blade.php
@@ -6,12 +6,12 @@
                     <div class="">
                     <div class="">Admin</div>
                     <div style="background: grey; padding: 10px 20px; margin-bottom: 10px;">
-            @else
-                <div class="w-100 d-flex justify-content-start" style="margin-bottom: 20px;">
-                    <div class="">
-                        <div class="">{{$message->user()->get()->first()->username}}</div>
-                    <div style="background: #32a89d; padding: 10px 20px; margin-bottom: 10px;">
-            @endif
+                        @else
+                            <div class="w-100 d-flex justify-content-start" style="margin-bottom: 20px;">
+                                <div class="">
+                                    <div class="">{{$message->user()->get()->first()->username}}</div>
+                                <div style="background: #32a89d; padding: 10px 20px; margin-bottom: 10px;">
+                        @endif
 
                         @if($message->type=='callback')
                             Нажал на {{$message->data['text']}}
@@ -21,11 +21,7 @@
                             {{$message->data['text']}}
                         @endif
                     </div>
-                        @if($message->type=='admin')
                             <div class="">{{$message->created_at}}</div>
-                        @else
-                            <div class="">{{$message->created_at}}</div>
-                        @endif
             </div>
         </div>
 @endforeach
