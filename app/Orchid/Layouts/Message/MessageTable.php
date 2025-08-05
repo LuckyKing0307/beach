82f2685048e75f88b90ba377e3d742febe356218
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-05 19:28:50 +0500

    Items Btns

--- app/Orchid/Layouts/Message/MessageTable.php
+++ app/Orchid/Layouts/Message/MessageTable.php
@@ -37,7 +37,7 @@ class MessageTable extends Table
             TD::make('User', 'USERNAME')
                 ->render(function (Message $message) {
                     $active = TelegramUser::where(['user_id'=>$message->user_id])->get()->first();
-                    return $active->username;
+                    return $active->username ?? $active->username :: $active->first_name;
                 }),
             TD::make('message', 'MESSAGE')->render(function (Message $message){
                 $text = Str::limit($message->data['text'], 30, '...');
